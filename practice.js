d0bfae144d75ea9b227005ed04f1958eec2dff40
saba-8009 <sabap0642@gmail.com>
2025-07-22 09:47:09 +0530

    its is my to do list

---
+++ practice.js
@@ -0,0 +1,13 @@
+const heading = document.querySelector("#myName");
+heading.style.color="red";
+ const head = document.querySelector(".heading-H-one");
+  head.style.color = "purple";
+
+
+  let honetag = document.querySelector(".heading-H-one");
+  honetag.textContent = " it's my javascript page for practice";
+  let btn = document.querySelector("button");
+  btn.style.backgroundColor = "green";
+  let MyTitle = document.querySelector("#title");
+  MyTitle.innerHTML = "<h2 style='color:brown;'>hello<br>Saba</h2>";
+  
